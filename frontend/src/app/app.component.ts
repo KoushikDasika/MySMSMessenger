d97c612a42be82251e48349c0649c058df48da0b
Koushik Dasika <koushikdasika@gmail.com>
2025-03-05 18:23:15 -0500

    form component (#14)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MessageListComponent } from '@/app/message-list/message-list.component';
+import { MessageFormComponent } from '@/app/message-form/message-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MessageListComponent],
+  imports: [RouterOutlet, MessageListComponent, MessageFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
