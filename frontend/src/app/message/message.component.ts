89075c13482048557d9a282c7998ee4eaa84805a
Koushik Dasika <koushikdasika@gmail.com>
2025-03-06 07:23:04 -0500

    remove stray console log

--- frontend/src/app/message/message.component.ts
+++ frontend/src/app/message/message.component.ts
@@ -19,7 +19,6 @@ export class MessageComponent {
   }
   
   formatPhoneNumber(phoneNumber: string) {
-    console.log('phoneNumber', phoneNumber);
     const parsed = parsePhoneNumber(phoneNumber);
     const significantNumber = parsed.number?.significant;
     if (!significantNumber) {
